661542147908d2410d8c29ac2538efb3e0539ebc
Alvaro Villatoro Morilla <avilmor3007@iescarrillo.es>
2025-10-01 12:26:50 +0200

    Listado de mesa

--- src/feature/table-list.ts
+++ src/feature/table-list.ts
@@ -6,3 +6,9 @@ numero: number;
 capacidad: number;
 ocupada: boolean;
 }
+
+let mesas: Mesa[] = [
+{ numero: 1, capacidad: 2, ocupada: false },
+{ numero: 2, capacidad: 4, ocupada: true },
+{ numero: 3, capacidad: 6, ocupada: false }
+];
